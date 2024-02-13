df1805d5155d15f4e544e106d065ee1430863cdd
Pr1jo <113903383+Pr1jo@users.noreply.github.com>
2024-02-13 10:29:06 +0100

    good

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header'
+import
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World!</h1>
+     {/* <Header /> */}
     </div>
   );
 }
